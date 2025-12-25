38cac97734cce94096b9bcfffc2881538b937294
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-12-25 14:55:38 +0800

    update server.js to add get method for home page

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/home', (request, response) => {
+    response.send("hi, this is the home page!")
+})
+
 app.listen(4000, () => {
     console.log("the server is running!")
 })
