d891ebd12e0c130a05a8709e5e0a16347bd5a6ab
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-12-25 14:58:35 +0800

    update server.js to add get method for announcement page

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/home', (request, response) => {
     response.send("hi, this is the home page!")
 })
 
+app.get('/announcement', (request, response) => {
+    response.send("this is the announcement page!")
+})
+
 app.listen(4000, () => {
     console.log("the server is running!")
 })
