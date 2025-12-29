d01dbbd70e27f3b71def0e90f908f4b2ae2cf956
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-12-29 15:54:12 +0800

    update productsRoutes.js to add respective router handlers

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -2,4 +2,20 @@ import express from "express"
 
 const router = express.Router();
 
-export default router
+router.get("/", (request, response) => {
+    response.status(200).send("You just fetched the products!");
+});
+
+router.post("/", (request, response) => {
+    response.status(201).json({message: "You just created a product!"});
+});
+
+router.put("/:id", (request, reponse) => {
+    response.status(200).json({message: "You just update a product!"});
+});
+
+router.delete("/:id", (request, response) => {
+    response.status(200).json({message: "You just delete a produce!"});
+});
+
+export default router;
