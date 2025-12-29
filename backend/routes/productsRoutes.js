b75d80e826c9611967252196012152812d3c7e88
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-12-29 16:11:03 +0800

    update productsRoutes.js for maintainability

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,21 +1,11 @@
 import express from "express"
+import { createProduct, deleteProduct, getAllNodes, getAllProduct, updateProduct } from "../controllers/productsControllers.js"
 
 const router = express.Router();
 
-router.get("/", (request, response) => {
-    response.status(200).send("You just fetched the products!");
-});
-
-router.post("/", (request, response) => {
-    response.status(201).json({message: "You just created a product!"});
-});
-
-router.put("/:id", (request, reponse) => {
-    response.status(200).json({message: "You just update a product!"});
-});
-
-router.delete("/:id", (request, response) => {
-    response.status(200).json({message: "You just delete a product!"});
-});
+router.get("/", getAllProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
