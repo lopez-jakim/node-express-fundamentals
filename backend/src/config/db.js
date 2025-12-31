150029798c695a272e963cc4c82c9eae2c7cc427
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-12-31 09:49:51 +0800

    create a db connection in db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,14 @@
 import mysql2 from "mysql2"
+
+const connectDB = async() => {
+    try {
+        const connection = await mysql2.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "1028",
+            database: "accreditrack_db",
+        });
+    } catch (error) {
+        console.error("Error connecting to MySQL:", error.message);
+    }
+};
