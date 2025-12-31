6196b7e3cff1e4a14e8ca8b5424c331f99e2721c
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-12-31 09:53:24 +0800

    import db.js into server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import cors from "cors"; // same as const cors = require('cors')
 import express from "express"; // same as const express = require('express')
 import productsRoutes from "./routes/productsRoutes.js"
+import connectDB from "./config/db.js"
 
 const app = express();
+
+connectDB()
 
 app.use(cors({
     origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
