4bb55ec8562b352be1e2c78b8476652905d8b882
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 03:10:28 +0800

    update Product.js to add respective data model for it

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,1 +1,22 @@
 import mysql2 from "mysql2"
+
+// 1- create a schema
+// 2- model based off of that schema
+
+const productSchema = new mysql2.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+    },
+    {timestamps: true} //createdAt, updatedAt
+);
+
+const Product = mysql2.model("Product", productSchema)
+
+export default Product
