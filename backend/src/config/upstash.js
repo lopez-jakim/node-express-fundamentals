9acc8b31da487d095db7df019b670400b78711e3
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 17:11:16 +0800

    add ratelimit instance

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,2 +1,9 @@
 import {Ratelimit} from "@upstash/ratelimit"
 import {Redis} from "@upstash/redis"
+
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(10, "20 s"),
+})
+
+export default ratelimit
