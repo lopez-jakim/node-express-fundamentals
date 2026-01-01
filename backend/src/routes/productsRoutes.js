8611b3769d4a9d4669ebdd72ccb17b73bc536323
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 16:18:53 +0800

    update productsRoutes.js t oadd getProductById route

--- backend/src/routes/productsRoutes.js
+++ backend/src/routes/productsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getAllProduct, createProduct, updateProduct, deleteProduct } from "../controllers/productsControllers.js"
+import { getAllProduct, createProduct, updateProduct, deleteProduct, getProductById } from "../controllers/productsControllers.js"
 
 const router = express.Router();
 
 router.get("/", getAllProduct);
+router.get("/", getProductById);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
