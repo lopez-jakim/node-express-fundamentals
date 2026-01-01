0710cb37c0b4a984ab366bf2dff21c3c1dbc1fbd
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 12:40:32 +0800

    update productsControllers.js to complete getAllProduct method

--- backend/src/controllers/productsControllers.js
+++ backend/src/controllers/productsControllers.js
@@ -1,6 +1,14 @@
+import product from "../models/Product.js"
+
 export async function getAllProduct(request, response) {
-    response.status(200).send("You just fetched the products!");
-}
+    try {
+        const products = await product.findAll()
+        response.status(200).json(products);
+    } catch(error) {
+        console.error("Error in getAllProducts Controller");
+        response.status(500).json({message: "Internal server error!"});
+    }
+}   
 
 export async function createProduct (request, response) {
     response.status(201).json({message: "You just created a product!"});
