bf8c08bf656186fc586cec8b77a4c9f5ec5ad7a8
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 10:30:12 +0800

    refactor db.js to apply sequelize

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,18 +1,22 @@
 import {Sequelize} from "sequelize";
 import dotenv from "dotenv";
 
+const sequelize = new Sequelize(
+    process.env.MYSQL_DB_NAME,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+    {
+    host: process.env.MYSQL_HOST,
+    dialect: process.env.DB_DIALECT,
+});
+
 const connectDB = async() => {
     try {
-        const connection = await mysql2.createConnection({
-            host: process.env.MYSQL_HOST,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_DB_NAME,
-        });
+        await Sequelize.authenticate();
         console.log("Connected to MySQL DataBase Successfully")
     } catch (error) {
         console.error("Error connecting to MySQL:", error.message);
     }
 };
 
-export default connectDB;
+export {sequelize, connectDB} ;
