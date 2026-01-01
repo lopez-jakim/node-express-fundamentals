ef088821a049cea5c8d4f83a07d5129758eb5ba1
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 14:38:41 +0800

    update server.js to add express.json middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,9 @@ app.use(cors({
     origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
 }));
 
+// middleware
+app.use(express.json())
+
 app.use("/api/products", productsRoutes);
 
 app.listen(4000, () => {
