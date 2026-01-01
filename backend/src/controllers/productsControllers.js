b434c34cee77a78adbb87fd1d27771f61234b554
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 15:06:50 +0800

    complete updateProduct controller in productsControllers.js

--- backend/src/controllers/productsControllers.js
+++ backend/src/controllers/productsControllers.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import product from "../models/Product.js"
 
 export async function getAllProduct(request, response) {
@@ -22,8 +23,25 @@ export async function createProduct (request, response) {
     }
 }
 
-export async function updateProduct (request, reponse) {
-    response.status(200).json({message: "You just update a product!"});
+export async function updateProduct (request, response) {
+    try {
+        const {title, content, price} = request.body;
+
+        // Sequelize's update() method
+        const [updatedRows] = await product.update(
+            {title, content, price },
+            {where: {id: request.params.id}}
+        );
+
+        if (updatedRows === 0) {
+            return response.status(404).json({message: "Product not found!"});
+        }
+
+        response.status(200).json({message: "Product updated successfully!"});
+    } catch(error) {
+        console.error("Error in updateProduct controller", error);
+        response.status(500).json({message: "Internal server error!"});
+    }
 }
 
 export async function deleteProduct (request, response) {
