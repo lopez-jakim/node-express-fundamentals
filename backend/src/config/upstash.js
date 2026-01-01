fe5c55488a112070d9272e3ee00a58959e224054
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 17:11:35 +0800

    import and add dotenv config

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,5 +1,8 @@
 import {Ratelimit} from "@upstash/ratelimit"
 import {Redis} from "@upstash/redis"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
