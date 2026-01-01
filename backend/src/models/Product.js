2ab9d6f8b496d732149681f8b6925caf37bee111
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2026-01-01 12:37:39 +0800

    update Product.js model to add id, price & timestamps attribute

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -4,9 +4,15 @@ import {sequelize} from "../config/db.js";
 // 1- create a schema
 // 2- model based off of that schema
 
-const Product = sequelize.define(
+const product = sequelize.define(
     "Product",
     {
+        id: {
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false // Required field
+        },
         title: {
             type: DataTypes.STRING,
             allowNull: false // Required field
@@ -15,8 +21,15 @@ const Product = sequelize.define(
             type: DataTypes.STRING,
             allowNull: false // Required field
         },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false // Required field
+        }
     },
-    {timestamps: true} //createdAt, updatedAt
+    {
+        tableName: process.env.TABLE_NAME,
+        timestamps: true //createdAt, updatedAt
+    } 
 );
 
-export default Product
+export default product
